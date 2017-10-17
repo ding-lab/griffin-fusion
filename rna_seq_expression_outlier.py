336a93699dbac08a411f5f75d4f6549465539796
envest <stevenmasonfoltz@gmail.com>
2017-10-17 17:54:35 -0500

    Editing to make work with real data

--- rna_seq_expression_outlier.py
+++ rna_seq_expression_outlier.py
@@ -10,27 +10,9 @@ for gene in gene_list_file:
 	gene_list.append(gene.strip())
 gene_list_file.close()
 
-output_file=sys.argv[3]
-
-try:
-  sys.argv[4]
-else NameError:
-	outlier_levels=1
-else:
-	if int(sys.argv[4]) and int(sys.argv[4])>0:
-    outlier_levels=int(sys.argv[4])
-  else:
-  	sys.exit("Outlier level parameter must be an integer > 0.")
-
-try:
-	sys.argv[5]
-else NameError:
-	input_file_header=False
-else:
-	input_file_header=sys.argv[5]
-
-if input_file_header:
-	input_file.readline()
+output_file=open(sys.argv[3],'w')
+outlier_levels=int(sys.argv[4])
+input_file_header=sys.argv[5]
 
 gene_dict = {}
 for gene in gene_list:
@@ -42,53 +24,62 @@ for gene in gene_list:
 sample_list = []
 
 for line in input_files:
+        print(line)
 	line = line.strip().split()
-  sample = line[0]
-  sample_file = open(line[1], 'r')
-  if sample in sample_list:
-  	sys.exit("Sample name "+sample+" appears at least twice in sample file list input file.")
-  else:
-  	sample_list.append(sample)
-  sample_dict = {}
-  for sline in sample_file:
-  	sline = sline.strip().split()
-  	gene = sline[0]
-  	expr = sline[1]
-  	if gene in sample_dict:
-  		sys.exit("Gene "+gene+" appears at least twice in sample "+sample+" input file.")
+	sample = line[0]
+  	sample_file = open(line[1], 'r')
+        if input_file_header:
+		sample_file.readline()
+  	if sample in sample_list:
+  		sys.exit("Sample name "+sample+" appears at least twice in sample file list input file.")
   	else:
-  	  sample_dict[gene] = float(expr)
-  for gene in gene_list:
-  	if gene in sample_dict:
-  	  gene_dict[gene][0].append(sample_dict[gene])
-    else:
-    	gene_dict[gene].append("NA")
-  sample_file.close()
+  		sample_list.append(sample)
+  	sample_dict = {}
+	for sline in sample_file:
+  		sline = sline.strip().split()
+  		gene = sline[1]
+  		expr = sline[3]
+		if gene in sample_dict:
+			sys.exit("Gene "+gene+" appears at least twice in sample "+sample+" input file.")
+  		else:
+			sample_dict[gene] = float(expr)
+  	for gene in gene_list:
+  		if gene in sample_dict:
+  	  		gene_dict[gene][0].append(sample_dict[gene])
+    		else:
+    			gene_dict[gene].append("NA")
+  	sample_file.close()
 input_files.close()
 
 for gene in gene_list:
+        print(gene)
 	expr_values = gene_dict[gene][0]
-	bc_expr_values, bc_lambda = stats.boxcox(expr_values)
+	print(expr_values)
+	#bc_expr_values, bc_lambda = stats.boxcox(expr_values)
+	bc_expr_values = ["NA" for x in range(len(expr_values))]
+	bc_lambda = "NA"
 	gene_dict[gene][1] = bc_expr_values
 	gene_dict[gene][2] = bc_lambda
 	pct75 = np.percentile(expr_values, 75)
 	pct25 = np.percentile(expr_values, 25)
 	iqr = pct75-pct25
 	overexpression_outlier_level_list = []
-  underexpression_outlier_level_list = []
-  for i in range(outlier_levels):
-    overexpression_outlier_level_list.append(pct75+1.5*iqr*(i+1))
-    underexpression_outlier_level_list.append(pct25-1.5*iqr*(i+1))
+  	underexpression_outlier_level_list = []
+  	for i in range(outlier_levels):
+    		overexpression_outlier_level_list.append(pct75+1.5*iqr*(i+1))
+    		underexpression_outlier_level_list.append(pct25-1.5*iqr*(i+1))
 
-output_file.write('\t'.join(['gene','sample','expression_level','boxcox_pct']+'\t'+'\t'.join(['overexpression'+str(x) for x in range(1,outlier_levels+1)])+'\t'.join(['underexpression'+str(x) for x in range(1,outlier_levels+1)]))+'\n')
+output_file.write( '\t'.join(['gene','sample','expression_level','boxcox_pct'])+'\t'+'\t'.join(['overexpression'+str(x) for x in range(1,outlier_levels+1)])+'\t'+'\t'.join(['underexpression'+str(x) for x in range(1,outlier_levels+1)])+'\n')
 for gene in gene_list:
 	for sample_number in range(len(sample_list)):
-		expr = gene_dict[gene][0][sample_number]
-		bc_pct = float(list(gene_dict[gene][1] <= gene_dict[gene][1][sample_number]).count(True))/len(sample_list)
-    output_list = [gene, sample_list[sample_number], expr, bc_pct]
-    for i in range(outlier_levels):
-    	output_list.append(int(expr > overexpression_outlier_level_list[i]))
-    for i in range(outlier_levels):
-    	output_list.append(int(expr < underexpression_outlier_level_list[i]))
-    output_file.write('\t'.join(output_list)+'\n')
+		expr = str(gene_dict[gene][0][sample_number])
+		#bc_pct = float(list(gene_dict[gene][1] <= gene_dict[gene][1][sample_number]).count(True))/len(sample_list)
+    		bc_pct = "NA"
+		output_list = [gene, sample_list[sample_number], expr, bc_pct]
+    		for i in range(outlier_levels):
+    			output_list.append(str(int(expr > overexpression_outlier_level_list[i])))
+    		for i in range(outlier_levels):
+    			output_list.append(str(int(expr < underexpression_outlier_level_list[i])))
+		print(output_list)
+    		output_file.write('\t'.join(output_list)+'\n')
 output_file.close()
