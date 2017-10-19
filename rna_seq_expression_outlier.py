6c8d5117ab185d7d64030caa1ea4ffbe96321c3f
envest <stevenmasonfoltz@gmail.com>
2017-10-19 11:54:06 -0500

    Fixed NA genes, group each sample when printing

--- rna_seq_expression_outlier.py
+++ rna_seq_expression_outlier.py
@@ -29,7 +29,6 @@ sample_list = []
 for line in input_files:
   line = line.strip().split()
   sample = line[0]
-  print("Sample "+sample)
   sample_file = open(line[1], 'r')
   if input_file_header:
     sample_file.readline()
@@ -50,41 +49,56 @@ for line in input_files:
       if gene in sample_dict:
         gene_dict[gene][0].append(sample_dict[gene])
       else:
-        gene_dict[gene].append("NA")
+        gene_dict[gene][0].append("NA")
   sample_file.close()
 input_files.close()
 
-gene_count=0.0
 for gene in gene_list:
-  gene_count += 1
-  print("Gene "+str(gene_count/len(gene_list)))
   if gene in gene_dict:
     expr_values = gene_dict[gene][0]
-    bc_expr_values, bc_lambda = stats.boxcox( [ x + 1e-9 for x in expr_values ] ) #add 1e-9 for 0-valued expression levels
-    bc_cdf = list( stats.norm.cdf( bc_expr_values ) )
-    gene_dict[gene][1] = bc_expr_values
-    gene_dict[gene][2] = bc_lambda
-    gene_dict[gene][3] = bc_cdf
-    pct75 = np.percentile(expr_values, 75)
-    pct25 = np.percentile(expr_values, 25)
-    iqr = pct75-pct25
-    overexpression_outlier_level_list = []
-    underexpression_outlier_level_list = []
-    for i in range(outlier_levels):
-      overexpression_outlier_level_list.append(pct75+1.5*iqr*(i+1))
-      underexpression_outlier_level_list.append(pct25-1.5*iqr*(i+1))
-    gene_dict[gene][4] = overexpression_outlier_level_list
-    gene_dict[gene][5] = underexpression_outlier_level_list
+    if all([ x == "NA" for x in expr_values]):
+      gene_dict[gene][1] = expr_values
+      gene_dict[gene][2] = "NA"
+      gene_dict[gene][3] = expr_values
+      gene_dict[gene][4] = "NA"
+      gene_dict[gene][5] = "NA"
+    else:
+      if all([ x == 0 for x in expr_values]):
+        bc_expr_values = expr_values
+        bc_lambda = "NA"
+      else:
+        bc_expr_values, bc_lambda = stats.boxcox( [ x + 1e-9 for x in expr_values ] ) #add 1e-9 for 0-valued expression levels
+      bc_cdf = list( stats.norm.cdf( bc_expr_values ) )
+      gene_dict[gene][1] = bc_expr_values
+      gene_dict[gene][2] = bc_lambda
+      gene_dict[gene][3] = bc_cdf
+      pct75 = np.percentile(expr_values, 75)
+      pct25 = np.percentile(expr_values, 25)
+      iqr = pct75-pct25
+      overexpression_outlier_level_list = []
+      underexpression_outlier_level_list = []
+      for i in range(outlier_levels):
+        overexpression_outlier_level_list.append(pct75+1.5*iqr*(i+1))
+        underexpression_outlier_level_list.append(pct25-1.5*iqr*(i+1))
+      gene_dict[gene][4] = overexpression_outlier_level_list
+      gene_dict[gene][5] = underexpression_outlier_level_list
 
 output_file.write( '\t'.join(['gene','sample','expression_level','percentile','boxcox_z', 'boxcox_l','boxcox_cdf'])+'\t'+'\t'.join(['overexpression'+str(x) for x in range(1,outlier_levels+1)])+'\t'+'\t'.join(['underexpression'+str(x) for x in range(1,outlier_levels+1)])+'\n')
-for gene in sorted(gene_dict.keys()):
-  for sample_number in range(len(sample_list)):
+for sample_number in range(len(sample_list)):
+  for gene in sorted(gene_dict.keys()):
+    gene_na = all([x == "NA" for x in gene_dict[gene][0]])
     expr = gene_dict[gene][0][sample_number]
-    pct = float( [ x <= gene_dict[gene][0][sample_number] for x in gene_dict[gene][0] ].count(True) )/len(sample_list)
+    if gene_na:
+      pct = "NA"
+    else:
+      pct = float( [ x <= expr for x in gene_dict[gene][0] ].count(True) )/len(sample_list)
     output_list = [gene, sample_list[sample_number], str(expr), str(pct), str(gene_dict[gene][1][sample_number]), str(gene_dict[gene][2]), str(gene_dict[gene][3][sample_number])]
-    for i in range(outlier_levels):
-      output_list.append(str(int(expr > gene_dict[gene][4][i])))
-    for i in range(outlier_levels):
-      output_list.append(str(int(expr < gene_dict[gene][5][i])))
+    if gene_na:
+      output_list.extend(["NA"]*outlier_levels*2)
+    else:
+      for i in range(outlier_levels):
+        output_list.append(str(int(expr > gene_dict[gene][4][i])))
+      for i in range(outlier_levels):
+        output_list.append(str(int(expr < gene_dict[gene][5][i])))
     output_file.write('\t'.join(output_list)+'\n')
 output_file.close()
