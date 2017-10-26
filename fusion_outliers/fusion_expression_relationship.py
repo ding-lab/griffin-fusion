eeb2eb3888ae4d7b4fa0f78cc93bcad980989963
envest <stevenmasonfoltz@gmail.com>
2017-10-26 17:00:59 -0500

    Test for association between fusion status and gene expression

---
+++ fusion_outliers/fusion_expression_relationship.py
@@ -0,0 +1,65 @@
+import sys
+from scipy import stats
+
+input_files = open(sys.argv[1],'r')
+outlier_level = sys.argv[2]
+
+input_file_list = []
+for input_file in input_files:
+  input_file_list.append(input_file.strip())
+input_files.close()
+n_samples = len(input_file_list)
+
+gene_dict = {}
+for i in range(n_samples):
+  file=open(input_file_list[i].strip(),'r')
+  first_line = file.readline().strip().split()
+  outlier_index = first_line.index("overexpression"+outlier_level)
+  fusion_index = first_line.index("fusion_info")
+  for line in file:
+    line = line.strip().split()
+    gene = line[0]
+    expr = float(line[2])
+    outlier = line[outlier_index]
+    fusion = line[fusion_index]
+    if gene not in gene_dict:
+      gene_dict[gene] = [[None]*n_samples, [None]*n_samples, [None]*n_samples, False, False]
+    gene_dict[gene][0][i] = expr
+    gene_dict[gene][1][i] = outlier
+    gene_dict[gene][2][i] = fusion
+  file.close()
+
+
+for k,v in gene_dict.items():
+  #t-test expression levels between fusion groups
+  fusion_exp = []
+  nonfusion_exp = []
+  for a,b in zip(v[0], v[2]):
+    if b == "None":
+      nonfusion_exp.append(a)
+    elif b == "Fusion_file_NA":
+      pass
+    else:
+      fusion_exp.append(a)
+  t, gene_dict[k][3] = scipy.stats.ttest_ind(fusion_exp, nonfusion_exp, equal_var=False)
+  #non-parametric test outlier status and fusion status
+  fus_out, fus_notout, notfus_out, notfus_notout = [0]*4
+  for a,b in zip(v[1],v[2]):
+    if a == 0 and b == "None":
+      notfus_notout += 1
+    elif a == 1 and b == "None":
+      notfus_out += 1
+    elif a == 0 and b != "Fusion_file_NA":
+      fus_notout += 1
+    elif a == 1 and b != "Fusion_file_NA":
+      fus_out += 1
+  oddsratio, gene_dict[k][4] = scipy.stats.fisher_exact([[notfus_notout,notfus_out],[fus_notout,fus_out]])
+
+sig_genes = []
+for k,v in gene_dict.items():
+  if v[3] < 0.05 or v[4] < 0.05:
+    sig_genes.append(k)
+
+for gene in sorted(sig_genes):
+  print('\t'.join([gene, str(gene_dict[gene][3]), str(gene_dict[gene][4])]))
+
