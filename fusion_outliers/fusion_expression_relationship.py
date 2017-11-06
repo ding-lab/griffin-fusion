649b3f81cb896099139c693353a0deda29cd9e8c
envest <stevenmasonfoltz@gmail.com>
2017-11-06 12:26:16 -0600

    Updated significance testing: under or over expression, percentiles

--- fusion_outliers/fusion_expression_relationship.py
+++ fusion_outliers/fusion_expression_relationship.py
@@ -1,9 +1,10 @@
 import sys
 import math
+import numpy as np
 from scipy import stats
 
-import warnings
-warnings.simplefilter('error')
+#import warnings
+#warnings.simplefilter('error')
 
 input_files = open(sys.argv[1],'r')
 outlier_level = sys.argv[2]
@@ -18,29 +19,40 @@ gene_dict = {}
 for i in range(n_samples):
   file=open(input_file_list[i].strip(),'r')
   first_line = file.readline().strip().split()
-  outlier_index = first_line.index("overexpression"+outlier_level)
+  gene_index = first_line.index("gene")
+  expression_index = first_line.index("expression_level")
+  percentile_index = first_line.index("percentile")
+  over_outlier_index = first_line.index("overexpression"+outlier_level)
+  under_outlier_index = first_line.index("underexpression"+outlier_level)
   fusion_index = first_line.index("fusion_info")
   for line in file:
     line = line.strip().split()
-    gene = line[0]
-    if line[2] == "NA":
+    gene = line[gene_index]
+    if line[expression_index] == "NA":
       pass
     else:
-      expr = float(line[2])
-      outlier = int(line[outlier_index])
+      expr = float(line[expression_index])
+      over_outlier = int(line[over_outlier_index])
+      under_outlier = int(line[under_outlier_index])
       fusion = line[fusion_index]
+      pct = line[percentile_index]
       if gene not in gene_dict:
-        gene_dict[gene] = [[None]*n_samples, [None]*n_samples, [None]*n_samples, None, None, None, None]
+        #gene_dict[gene] = [ 0.expression , 1.overexpression, 2.underexpression, 3.fusion, 4.t-test, 5.mann-whitney-u, 
+        #                    6.fishers exact over, 7.fishers exact under, 8.num fusion over outliers, 9.num fusion under outliers, 10.total samples with fusion data
+        #                    11. percentiles, 12. fusion percentile median ]
+        gene_dict[gene] = [[None]*n_samples, [None]*n_samples, [None]*n_samples, [None]*n_samples, None, None, None, None, None, None, None, [None]*n_samples]
       gene_dict[gene][0][i] = expr
-      gene_dict[gene][1][i] = outlier
-      gene_dict[gene][2][i] = fusion
+      gene_dict[gene][1][i] = over_outlier
+      gene_dict[gene][2][i] = under_outlier
+      gene_dict[gene][3][i] = fusion
+      gene_dict[gene][11][i] = pct
   file.close()
 
 for k,v in gene_dict.items():
   #t-test expression levels between fusion groups
   fusion_exp = []
   nonfusion_exp = []
-  for a,b in zip(v[0], v[2]):
+  for a,b in zip(v[0], v[3]):
     if b == "None_reported":
       nonfusion_exp.append(a)
     elif b == "Fusion_file_NA":
@@ -48,31 +60,74 @@ for k,v in gene_dict.items():
     else:
       fusion_exp.append(a)
   if len(fusion_exp) < 2 or len(nonfusion_exp) < 2:
-    t, gene_dict[k][3] = [float('NaN'), float('NaN')]
-    mwu, gene_dict[k][4] = [float('NaN'), float('NaN')]
+    t, gene_dict[k][4] = [float('NaN'), float('NaN')]
+    mwu, gene_dict[k][5] = [float('NaN'), float('NaN')]
   else:
-    t, gene_dict[k][3] = stats.ttest_ind(fusion_exp, nonfusion_exp, axis=0, equal_var=False)
+    t, gene_dict[k][4] = stats.ttest_ind(fusion_exp, nonfusion_exp, axis=0, equal_var=False)
     #Mann-Whitney U test
-    mwu, gene_dict[k][4] = stats.mannwhitneyu(fusion_exp, nonfusion_exp, alternative='two-sided')
-  #non-parametric test outlier status and fusion status
-  fus_out, fus_notout, notfus_out, notfus_notout = [0]*4
-  for a,b in zip(v[1],v[2]):
+    mwu, gene_dict[k][5] = stats.mannwhitneyu(fusion_exp, nonfusion_exp, alternative='two-sided')
+  #non-parametric test overexpression outlier status and fusion status
+  fus_overout, fus_notoverout, notfus_overout, notfus_notoverout = [0]*4
+  for a,b in zip(v[1],v[3]):
     if a == 0 and b == "None_reported":
-      notfus_notout += 1
+      notfus_notoverout += 1
     elif a == 1 and b == "None_reported":
-      notfus_out += 1
+      notfus_overout += 1
     elif a == 0 and b != "Fusion_file_NA":
-      fus_notout += 1
+      fus_notoverout += 1
     elif a == 1 and b != "Fusion_file_NA":
-      fus_out += 1
-  oddsratio, gene_dict[k][5] = stats.fisher_exact([[notfus_notout,notfus_out],[fus_notout,fus_out]])
-  gene_dict[k][6] = fus_out
-
-sig_genes = []
-for k,v in gene_dict.items():
-  if (not math.isnan(v[3]) and v[3] < 0.05) or (not math.isnan(v[4]) and v[4] < 0.05) or (v[5] < 0.05 and gene_dict[k][6] > 0):
-    sig_genes.append(k)
-
+      fus_overout += 1
+  oddsratio, gene_dict[k][6] = stats.fisher_exact([[notfus_notoverout,notfus_overout],[fus_notoverout,fus_overout]])
+  gene_dict[k][8] = fus_overout
+  #non-parametric test underexpression outlier status and fusion status
+  fus_underout, fus_notunderout, notfus_underout, notfus_notunderout = [0]*4
+  for a,b in zip(v[2],v[3]):
+    if a == 0 and b == "None_reported":
+      notfus_notunderout += 1
+    elif a == 1 and b == "None_reported":
+      notfus_underout += 1
+    elif a == 0 and b != "Fusion_file_NA":
+      fus_notunderout += 1
+    elif a == 1 and b != "Fusion_file_NA":
+      fus_underout += 1
+  oddsratio, gene_dict[k][7] = stats.fisher_exact([[notfus_notunderout,notfus_underout],[fus_notunderout,fus_underout]])
+  gene_dict[k][9] = fus_underout
+  gene_dict[k][10] = notfus_notunderout + notfus_underout + fus_notunderout + fus_underout
+  #Fusion status and percertile
+  fus_pct = []
+  for a,b in zip(v[3],v[11]):
+    if a == "None_reported":
+      pass
+    elif a != "Fusion_file_NA":
+      fus_pct.append(b)
+  gene_dict[k][12] = float(np.median(fus_pct))
+ 
+multiple_test_corrected_pvalue = 0.05/len(gene_dict.keys())
 for gene in sorted(sig_genes):
-  print('\t'.join([gene, str(gene_dict[gene][3]), str(gene_dict[gene][4]), str(gene_dict[gene][5]), str(gene_dict[gene][6])]))
+  print_gene=False
+  v = gene_dict[gene]
+  ttest, mwutest, overfisher, underfisher, fusionpct = ["NS"]*5
+  #t-test
+  if not math.isnan(v[4]) and v[4] < multiple_test_corrected_pvalue:
+    ttest = v[4]
+    print_gene=True
+  #MWU test
+  if not math.isnan(v[5]) and v[5] < multiple_test_corrected_pvalue:
+    mwutest = v[5]
+    print_gene=True
+  #Fisher Exact -- overexpression
+  if v[6] < multiple_test_corrected_pvalue:
+    overfisher = v[6]
+    print_gene=True
+  #Fisher Exact -- underexpression
+  if v[7] < multiple_test_corrected_pvalue:
+    underfisher = v[7]
+    print_gene=True
+  #Fusion percentile
+  if v[12] >= 0.90 or v[12] <= 0.10:
+    fusionpct = v[12]
+    print_gene=True
+
+  if print_gene:
+    print('\t'.join([str(x) for x in [gene, ttest, mwutest, overfisher, underfisher, fusionpct]]))
 
