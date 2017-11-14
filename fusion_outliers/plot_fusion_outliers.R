87629791cfb422c666213bd3ada3a58bcbb47eb8
envest <stevenmasonfoltz@gmail.com>
2017-11-14 14:07:17 -0600

    Updated plots to show CNV as color, label entire fusion name

--- fusion_outliers/plot_fusion_outliers.R
+++ fusion_outliers/plot_fusion_outliers.R
@@ -11,38 +11,25 @@ ylabel <- args[5]
 
 gene_id <- df$gene[1]
 
-df$cnv <- factor(df$cnv)
+levels(df$cnv) <- c("Deep Deletion","Shallow Deletion","Neutral","Low Amplification","High Amplification","No Data")
+df$cnv[df$cnv=="-2"] <- "Deep Deletion"
+df$cnv[df$cnv=="-1"] <- "Shallow Deletion"
+df$cnv[df$cnv=="0"] <- "Neutral"
+df$cnv[df$cnv=="1"] <- "Low Amplification"
+df$cnv[df$cnv=="2"] <- "High Amplification"
+df$cnv[is.na(df$cnv)] <- "No Data"
+df$cnv <- factor(df$cnv, levels=c("Deep Deletion","Shallow Deletion","Neutral","Low Amplification","High Amplification","No Data"))
 
 fusion_status <- factor((df$fusion_info == "None_reported") + 2*(df$fusion_info == "Fusion_file_NA"))
-levels(fusion_status) <- c("Fusion", "None reported", "NA")
+levels(fusion_status) <- c("Fusion", "None reported", "No Data")
 
-five_prime_three_prime <- factor(rep(NA,nrow(df)))
-levels(five_prime_three_prime) <- c("5' and 3'", "5' only", "3' only", "None", "NA")
 gene_partners <- rep(NA,nrow(df))
 
 for(i in 1:nrow(df)){
-  if(as.character(df$fusion_info[i]) == "None_reported"){
-    five_prime_three_prime[i] <- "None"
-  } else if(as.character(df$fusion_info[i]) == "Fusion_file_NA"){
-    five_prime_three_prime[i] <- "NA"
-  } else{
-    a <- (as.character(df$gene[i]) %in% strsplit(as.character(df$geneA[i]),';')[[1]])
-    b <- (as.character(df$gene[i]) %in% strsplit(as.character(df$geneB[i]),';')[[1]])
-    if(a & b){
-      five_prime_three_prime[i] <- "5' and 3'"
-      all_partners <- unique(sort(c(unique(sort(strsplit(as.character(df$geneA[i]),';')[[1]])), unique(sort(strsplit(as.character(df$geneB[i]),';')[[1]])))))
-      gene_partners[i] <- paste(all_partners[all_partners != as.character(df$gene[i])], collapse=",")
-    } else if(a & !b){
-      five_prime_three_prime[i] <- "5' only"
-      all_partners <- unique(sort(strsplit(as.character(df$geneB[i]),';')[[1]]))
-      gene_partners[i] <- paste(all_partners[all_partners != as.character(df$gene[i])], collapse=",")
-    } else if(!a & b){
-      five_prime_three_prime[i] <- "3' only"
-      all_partners <- unique(sort(strsplit(as.character(df$geneA[i]),';')[[1]]))
-      gene_partners[i] <- paste(all_partners[all_partners != as.character(df$gene[i])], collapse=",")
-    } else{
-      exit("Fusion but neither 5' nor 3'")
-    }
+  if( !(as.character(df$fusion_info[i]) %in% c("None_reported","Fusion_file_NA")) ){
+    genes_a <- strsplit(as.character(df$geneA[i]),';')[[1]]
+    genes_b <- strsplit(as.character(df$geneB[i]),';')[[1]]
+    gene_partners[i] <- paste( paste(genes_a,genes_b,sep="--"), collapse='\n')
   }
 }
 
@@ -50,22 +37,21 @@ set.seed(10)
 
 outlier_status <- apply(df[,c(over_outlier_level, under_outlier_level)], 1, sum)
 alpha_level <- outlier_status/2 + 0.5
-plot_df <- data.frame(df[,c("gene","cnv","expression_level")], fusion_status=fusion_status, fusion_status_j=jitter(as.numeric(fusion_status)), alpha_level=alpha_level, five_prime_three_prime, gene_partners=gene_partners)
+plot_df <- data.frame(df[,c("gene","cnv","expression_level")], fusion_status=fusion_status, fusion_status_j=jitter(as.numeric(fusion_status)), alpha_level=alpha_level, gene_partners=gene_partners)
 
 library(ggplot2)
 library(ggrepel)
 
-#p <- ggplot(plot_df, aes(x=fusion_status, y=expression_level, color=five_prime_three_prime))
-p <- ggplot(plot_df, aes(x=fusion_status, y=expression_level, color=cnv))
-p <- p + geom_violin(color="black")
-p <- p + geom_point(data=subset(plot_df, fusion_status != "Fusion"), aes(x=fusion_status_j, alpha=alpha_level))
-p <- p + geom_text(data=subset(plot_df, fusion_status == "Fusion"), aes(x=fusion_status_j, alpha=alpha_level, label=five_prime_three_prime))
-p <- p + geom_text_repel(data=subset(plot_df, fusion_status=="Fusion"), aes(x=fusion_status_j, y=expression_level, label=gene_partners, alpha=alpha_level), show.legend=FALSE)
+p <- ggplot(plot_df, aes(x=fusion_status, y=expression_level, color=cnv, fill=cnv))
+p <- p + geom_violin(aes(fill=NULL),color="black")
+p <- p + geom_point(aes(x=fusion_status_j, alpha=alpha_level))
+p <- p + geom_label_repel(data=subset(plot_df, fusion_status=="Fusion"), aes(x=fusion_status_j, y=expression_level, label=gene_partners), label.size=NA, color=c('green','black')[as.numeric(subset(plot_df, fusion_status=="Fusion")$cnv=="No Data")+1], box.padding=0.35, point.padding=0.5, segment.color="grey50")
 p <- p + ylim(0, max(df$expression_level))
 p <- p + theme_bw(base_size=20)
-p <- p + scale_color_brewer(palette="Set1", drop=FALSE) #, breaks=c("5' and 3'", "5' only", "3' only"))
-p <- p + labs(x="Fusion Status", y=ylabel, title=title, color=paste0(gene_id,"\nPosition"))
-p <- p + guides(alpha=FALSE)
+p <- p + scale_color_brewer(palette="Set1", drop=FALSE)
+p <- p + scale_fill_brewer(palette="Set1", drop=FALSE)
+p <- p + labs(x="Fusion Status", y=ylabel, title=title, color="CNV status")
+p <- p + guides(fill=FALSE, alpha=FALSE)
 
 pdf(plot_file, 10, 10)
 p
