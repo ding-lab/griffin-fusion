8fa0ccd1fc110845b6486432d58e6b3148bb2717
envest <stevenmasonfoltz@gmail.com>
2017-11-18 14:51:47 -0600

    Only plot if one or more fusions, change alpha to .75 for all points, draw median line

--- fusion_outliers/plot_fusion_outliers.R
+++ fusion_outliers/plot_fusion_outliers.R
@@ -20,8 +20,12 @@ df$cnv[df$cnv=="2"] <- "High Amplification"
 df$cnv[is.na(df$cnv)] <- "No Data"
 df$cnv <- factor(df$cnv, levels=c("Deep Deletion","Shallow Deletion","Neutral","Low Amplification","High Amplification","No Data"))
 
-fusion_status <- factor((df$fusion_info == "None_reported") + 2*(df$fusion_info == "Fusion_file_NA"))
-levels(fusion_status) <- c("Fusion", "None reported", "No Data")
+#fusion_status <- factor((df$fusion_info == "None_reported") + 2*(df$fusion_info == "Fusion_file_NA"))
+#levels(fusion_status) <- c("Fusion", "None reported", "No Data")
+
+fusion_status_num <- 1 + as.numeric(df$fusion_info == "None_reported") + 2*as.numeric(df$fusion_info == "Fusion_file_NA")
+fusion_status <- factor(c("Fusion","None reported","No Data")[fusion_status_num], levels=c("Fusion","None reported","No Data"), ordered=TRUE)
+
 
 gene_partners <- rep(NA,nrow(df))
 
@@ -39,21 +43,27 @@ outlier_status <- apply(df[,c(over_outlier_level, under_outlier_level)], 1, sum)
 alpha_level <- outlier_status/2 + 0.5
 plot_df <- data.frame(df[,c("gene","cnv","expression_level")], fusion_status=fusion_status, fusion_status_j=jitter(as.numeric(fusion_status)), alpha_level=alpha_level, gene_partners=gene_partners)[df$fusion_info != "Fusion_file_NA",]
 
-library(ggplot2)
-library(ggrepel)
+if( any(plot_df$fusion_status == "Fusion") ){
+  library(ggplot2)
+  library(ggrepel)
+  #fusion_median <- median(subset(plot_df, fusion_status=="Fusion")$expression_level)
+  #nonfusion_median <- median(subset(plot_df, fusion_status=="None reported")$expression_level)
+  #median_df <- data.frame(fusion_status=c("Fusion","None reported"), median=c(fusion_median, nonfusion_median))
 
-p <- ggplot(plot_df, aes(x=fusion_status, y=expression_level, color=cnv, fill=cnv))
-p <- p + geom_violin(aes(fill=NULL),color="black")
-p <- p + geom_point(aes(x=fusion_status_j, alpha=alpha_level))
-p <- p + geom_label_repel(data=subset(plot_df, fusion_status=="Fusion"), aes(x=fusion_status_j, y=expression_level, label=gene_partners), label.size=NA, color=c('white','black')[as.numeric(subset(plot_df, fusion_status=="Fusion")$cnv=="No Data")+1], box.padding=0.35, point.padding=0.5, segment.color="grey50")
-p <- p + ylim(0, max(df$expression_level))
-p <- p + theme_bw(base_size=20)
-p <- p + scale_color_brewer(palette="Set1", drop=FALSE)
-p <- p + scale_fill_brewer(palette="Set1", drop=FALSE)
-p <- p + labs(x="Fusion Status", y=ylabel, title=title, color="CNV status")
-p <- p + guides(fill=FALSE, alpha=FALSE)
+  p <- ggplot(plot_df, aes(x=fusion_status, y=expression_level, color=cnv, fill=cnv))
+  
+  p <- p + geom_violin(aes(fill=NULL),color="black", draw_quantiles=c(0.5))
+  p <- p + geom_point(aes(x=fusion_status_j), alpha=0.75) #, alpha=alpha_level))
+  p  <- p + geom_label_repel(data=subset(plot_df, fusion_status=="Fusion"), aes(x=fusion_status_j, y=expression_level, label=gene_partners), label.size=NA, color=c('white','black')[as.numeric(subset(plot_df, fusion_status=="Fusion")$cnv=="No Data")+1], box.padding=0.35, point.padding=0.5, segment.color="grey50")
+  p <- p + ylim(0, max(df$expression_level))
+  p <- p + theme_bw(base_size=20)
+  p <- p + scale_color_brewer(palette="Set1", drop=FALSE)
+  p <- p + scale_fill_brewer(palette="Set1", drop=FALSE)
+  p <- p + labs(x="Fusion Status", y=ylabel, title=title, color="CNV Status")
+  p <- p + guides(fill=FALSE, alpha=FALSE)
 
-pdf(plot_file, 10, 10, useDingbats = FALSE)
-p
-shh <- dev.off()
+  pdf(plot_file, 10, 10, useDingbats = FALSE)
+  print(p)
+  shh <- dev.off()
+}
 
