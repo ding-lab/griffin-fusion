5458f44f9ac46a5044efa72c329c034605bfd4c7
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-03-22 11:52:56 -0500

    Keep only code relevant to landscaping

--- mmy_fusion/analysis/landscape/landscape.R
+++ mmy_fusion/analysis/landscape/landscape.R
@@ -1,12 +1,4 @@
-setwd("/Users/sfoltz/Desktop/griffin-fusion/mmy_fusion/analysis")
-data="/Users/sfoltz/Desktop/griffin-fusion/mmy_fusion/data/"
 library(ggplot2)
-fusion_df <- read.table(paste0(data,"fusion_df.txt"),header=T)
-samples <- read.table(paste0(data,"sample_list.807.txt"), header=F)
-
-expr <- read.table(paste0(data,"mmy_gene_expr_with_fusions.tsv"),header=T)
-
-primary_df <- subset(fusion_df, sample_number==1)
 
 top_10_names <- names(sort(table(primary_df$fusion), decreasing=TRUE)[1:10])
 top_10_count <- as.vector(sort(table(primary_df$fusion), decreasing=TRUE)[1:10])
