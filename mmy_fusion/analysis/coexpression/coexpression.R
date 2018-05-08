c46059ece7bde0f14fa314252a241753a178b84c
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-05-08 10:11:01 -0500

    Removing non-primary samples from WHSC1/FGFR3 analysis

--- mmy_fusion/analysis/coexpression/coexpression.R
+++ mmy_fusion/analysis/coexpression/coexpression.R
@@ -5,8 +5,8 @@ srr_with_fgfr3_fusion <- unique(subset(primary_df, (geneB %in% c("FGFR3","IGH@")
 srr_with_whsc1_fusion <- unique(subset(primary_df, (geneB %in% c("WHSC1","IGH@") & geneA %in% c("WHSC1","IGH@")))$srr) #| (geneB %in% c("WHSC1") & geneA=="IGH@"))$srr)
 srr_with_t414 <- unique(subset(primary_df, primary_df$seqfish_Translocation_WHSC1_4_14 == 1)$srr)
 srr_NA_t414 <- unique(subset(primary_df, is.na(primary_df$seqfish_Translocation_WHSC1_4_14))$srr)
-fgfr3_log10tpm <- subset(expr, gene %in% c("FGFR3"))[,c("srr","log10tpm")]
-whsc1_log10tpm <- subset(expr, gene %in% c("WHSC1"))[,c("srr","log10tpm")]
+fgfr3_log10tpm <- subset(expr, gene %in% c("FGFR3") & srr %in% samples$V2)[,c("srr","log10tpm")]
+whsc1_log10tpm <- subset(expr, gene %in% c("WHSC1") & srr %in% samples$V2)[,c("srr","log10tpm")]
 has_fgfr3_fusion=(fgfr3_log10tpm[,1] %in% srr_with_fgfr3_fusion)
 has_whsc1_fusion=(fgfr3_log10tpm[,1] %in% srr_with_whsc1_fusion)
 has_t414=(fgfr3_log10tpm[,1] %in% srr_with_t414)
@@ -72,28 +72,35 @@ for(this_srr in igh_whsc1_df$Sample){
 }
 plot_df <- data.frame(bp_igh=bp_igh, bp_whsc1=bp_whsc1, ex_fgfr3=ex_fgfr3, ex_whsc1=ex_whsc1)
 
-ggplot(plot_df, aes(x=bp_igh, y=bp_whsc1, color=as.factor(round(ex_fgfr3)))) + geom_point()
+ggplot(plot_df, aes(x=bp_igh/1000, y=bp_whsc1/1000, color=as.factor(round(ex_fgfr3)))) + geom_point(size=3) + theme_bw(base_size=20) + labs(x="IGH Breakpoint (kb) (chromosome 14)", y="WHSC1 Breakpoint (kb) (chromosome 4)", color="Expression (rounded)\nlog10(TPM)", title="IGH--WHSC1 Fusion Genomic Breakpoints") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-srr_with_whsc1_fusion_plus_wgs <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$srr
-bpA <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$posA
-bpB <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$posB
-seqfish_t414 <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$seqfish_Translocation_WHSC1_4_14
-wgs_bam <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$wgs_bam
-wgs_reads <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$discordant_reads
-fgfr3_expr <- NULL
-fgfr3_pct <- NULL
-fgfr3_cnv <- NULL
-fgfr3_over <- NULL
-for(this_srr in srr_with_whsc1_fusion_plus_wgs){
-  this_line <- subset(expr, srr==this_srr & gene=="FGFR3")
-  fgfr3_expr <- c(fgfr3_expr, this_line$log10tpm)
-  fgfr3_pct <- c(fgfr3_pct, this_line$pct)
-  fgfr3_cnv <- c(fgfr3_cnv, this_line$gene_avg_cnv)
-  fgfr3_over <- c(fgfr3_over, this_line$outlier_over_log10tpm)
-}
-plot_df <- data.frame(srr=srr_with_whsc1_fusion_plus_wgs, breakpointA=bpA, breakpointB=bpB, fgfr3_expr=fgfr3_expr, fgfr3_cnv=fgfr3_cnv, fgfr3_pct=fgfr3_pct, seqfish_t414=as.factor(seqfish_t414), wgs_bam=wgs_bam, wgs_reads=wgs_reads, category=as.factor(round(fgfr3_expr)), over=as.factor(fgfr3_over))
-ggplot(plot_df, aes(x=as.factor(breakpointA), y=as.factor(breakpointB), color=category)) + geom_jitter(width=0.2, height=0.2) + theme_bw(base_size=20)
+ggplot(subset(plot_df, bp_igh > 106300000), aes(x=bp_igh/1000, y=bp_whsc1/1000, color=as.factor(round(ex_fgfr3)))) + geom_point(size=3) + theme_bw(base_size=20) + labs(x="IGH Breakpoint (kb) (chromosome 14)", y="WHSC1 Breakpoint (kb) (chromosome 4)", color="Expression (rounded)\nlog10(TPM)", title="IGH--WHSC1 Fusion Genomic Breakpoints") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+
+
+
+
+# ##################
+# srr_with_whsc1_fusion_plus_wgs <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$srr
+# bpA <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$posA
+# bpB <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$posB
+# seqfish_t414 <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$seqfish_Translocation_WHSC1_4_14
+# wgs_bam <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$wgs_bam
+# wgs_reads <- subset(primary_df, fusion=="IGH@--WHSC1" & !is.na(seqfish_Translocation_WHSC1_4_14))$discordant_reads
+# fgfr3_expr <- NULL
+# fgfr3_pct <- NULL
+# fgfr3_cnv <- NULL
+# fgfr3_over <- NULL
+# for(this_srr in srr_with_whsc1_fusion_plus_wgs){
+#   this_line <- subset(expr, srr==this_srr & gene=="FGFR3")
+#   fgfr3_expr <- c(fgfr3_expr, this_line$log10tpm)
+#   fgfr3_pct <- c(fgfr3_pct, this_line$pct)
+#   fgfr3_cnv <- c(fgfr3_cnv, this_line$gene_avg_cnv)
+#   fgfr3_over <- c(fgfr3_over, this_line$outlier_over_log10tpm)
+# }
+# plot_df <- data.frame(srr=srr_with_whsc1_fusion_plus_wgs, breakpointA=bpA, breakpointB=bpB, fgfr3_expr=fgfr3_expr, fgfr3_cnv=fgfr3_cnv, fgfr3_pct=fgfr3_pct, seqfish_t414=as.factor(seqfish_t414), wgs_bam=wgs_bam, wgs_reads=wgs_reads, category=as.factor(round(fgfr3_expr)), over=as.factor(fgfr3_over))
+# ggplot(plot_df, aes(x=as.factor(breakpointA), y=as.factor(breakpointB), color=category)) + geom_jitter(width=0.2, height=0.2) + theme_bw(base_size=20)
+# ##################
 
 
 
@@ -113,29 +120,28 @@ plot_df <- rbind(plot_df, data.frame(percentile=subset(primary_df, as.logical(ge
 
 ggplot(plot_df, aes(x=gene_type, y=percentile)) + geom_violin(size=2) + geom_jitter(data=plot_df, aes(color=pos), size=3, alpha=0.5) + theme_bw(base_size=20) + labs(x="Gene Category", y="Expression Percentile", title="Expression Percentile of Oncogenes and Tumor Suppressors", color="Gene\norientation")
 
-
-plot_df <- NULL
-oncogenes <- as.character(subset(primary_df, as.logical(geneA_oncogene))$geneA)
-oncogenes <- c(oncogenes, as.character(subset(primary_df, as.logical(geneB_oncogene))$geneB))
-oncogenes <- unique(oncogenes)
-for(g in oncogenes){
-  pct <- subset(primary_df, geneA==g)$geneA_pct
-  pct <- unique(c(pct, subset(primary_df, geneB==g)$geneB_pct))
-  plot_df <- rbind(plot_df, data.frame(gene_type="Oncogene", gene=g, n_samples=length(pct), median=median(pct)))
-}
-
-tsgs <- as.character(subset(primary_df, as.logical(geneA_tsg))$geneA)
-tsgs <- c(tsgs, as.character(subset(primary_df, as.logical(geneB_tsg))$geneB))
-tsgs <- unique(tsgs)
-for(g in tsgs){
-  pct <- subset(primary_df, geneA==g)$geneA_pct
-  pct <- unique(c(pct, subset(primary_df, geneB==g)$geneB_pct))
-  plot_df <- rbind(plot_df, data.frame(gene_type="Tumor Suppressor", gene=g, n_samples=length(pct), median=median(pct)))
-}
-
-gene_type_order <- c(order(subset(plot_df, gene_type=="Oncogene")$n_samples, decreasing=TRUE), order(subset(plot_df, gene_type=="Tumor Suppressor")$n_samples, decreasing=TRUE))
-plot_df <- data.frame(plot_df, gene_type_order=gene_type_order)
-
-
-ggplot(subset(plot_df, gene_type_order <= 10), aes(x=gene, y=gene_type, size=n_samples, color=median)) + geom_point(alpha=0.5) + scale_colour_gradient2(low=muted("blue"), high=muted("red"), midpoint = 0.5) + facet_grid( . ~ gene_type)
-
+# plot_df <- NULL
+# oncogenes <- as.character(subset(primary_df, as.logical(geneA_oncogene))$geneA)
+# oncogenes <- c(oncogenes, as.character(subset(primary_df, as.logical(geneB_oncogene))$geneB))
+# oncogenes <- unique(oncogenes)
+# for(g in oncogenes){
+#   pct <- subset(primary_df, geneA==g)$geneA_pct
+#   pct <- unique(c(pct, subset(primary_df, geneB==g)$geneB_pct))
+#   plot_df <- rbind(plot_df, data.frame(gene_type="Oncogene", gene=g, n_samples=length(pct), median=median(pct)))
+# }
+# 
+# tsgs <- as.character(subset(primary_df, as.logical(geneA_tsg))$geneA)
+# tsgs <- c(tsgs, as.character(subset(primary_df, as.logical(geneB_tsg))$geneB))
+# tsgs <- unique(tsgs)
+# for(g in tsgs){
+#   pct <- subset(primary_df, geneA==g)$geneA_pct
+#   pct <- unique(c(pct, subset(primary_df, geneB==g)$geneB_pct))
+#   plot_df <- rbind(plot_df, data.frame(gene_type="Tumor Suppressor", gene=g, n_samples=length(pct), median=median(pct)))
+# }
+# 
+# gene_type_order <- c(order(subset(plot_df, gene_type=="Oncogene")$n_samples, decreasing=TRUE), order(subset(plot_df, gene_type=="Tumor Suppressor")$n_samples, decreasing=TRUE))
+# plot_df <- data.frame(plot_df, gene_type_order=gene_type_order)
+# 
+# 
+# ggplot(subset(plot_df, gene_type_order <= 10), aes(x=gene, y=gene_type, size=n_samples, color=median)) + geom_point(alpha=0.5) + scale_colour_gradient2(low=muted("blue"), high=muted("red"), midpoint = 0.5) + facet_grid( . ~ gene_type)
+# 
