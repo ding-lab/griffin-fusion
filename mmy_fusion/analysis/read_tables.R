aa51ec8c85a1b81c0f20b9ebff9a8ef1c94ac53a
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-05-08 10:08:49 -0500

    Differentiate all_samples and samples (primary)

--- mmy_fusion/analysis/read_tables.R
+++ mmy_fusion/analysis/read_tables.R
@@ -2,6 +2,7 @@ setwd("/Users/sfoltz/Desktop/griffin-fusion/mmy_fusion/analysis")
 data="/Users/sfoltz/Desktop/griffin-fusion/mmy_fusion/data/"
 
 fusion_df <- read.table(paste0(data,"fusion_df.txt"),header=T)
-samples <- read.table(paste0(data,"sample_list.807.txt"), header=F)
+all_samples <- read.table(paste0(data,"sample_list.807.txt"), header=F)
+samples <- read.table(paste0(data,"sample_list.primary.txt"), header=F)
 expr <- read.table(paste0(data,"mmy_gene_expr_with_fusions.tsv"),header=T)
 primary_df <- subset(fusion_df, sample_number==1)
