bdd4e82b6a4b7fbf5bf70f01cd5f7492c8dc2edd
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-06-19 13:50:50 -0500

    Fix paths and remove reference to working directory

--- mmy_fusion/analysis/read_tables.R
+++ mmy_fusion/analysis/read_tables.R
@@ -1,12 +1,9 @@
-setwd("/Users/sfoltz/Desktop/griffin-fusion/mmy_fusion/analysis")
-data="/Users/sfoltz/Desktop/griffin-fusion/mmy_fusion/data/"
-
-fusion_df <- read.table(paste0(data,"fusion_df.txt"),header=T)
-all_samples <- read.table(paste0(data,"sample_list.807.txt"), header=F)
+fusion_df <- read.table("data/fusion_df.txt",header=T)
+all_samples <- read.table("data/sample_list.806.txt", header=F)
 names(all_samples) <- c("MMRF","SRR")
-samples <- read.table(paste0(data,"sample_list.primary.txt"), header=F)
+samples <- read.table("data/sample_list.primary.txt", header=F)
 names(samples) <- c("MMRF","SRR")
-expr <- read.table(paste0(data,"mmy_gene_expr_with_fusions.tsv"),header=T)
+expr <- read.table("data/mmy_gene_expr_with_fusions.tsv",header=T)
 primary_df <- subset(fusion_df, sample_number==1)
-clinical_df <- read.table(paste0(data,"clinical_df.tsv"), header=T, stringsAsFactors=FALSE)
-seqfish_df <- read.table(paste0(data,"seqfish_df.tsv"),header=T)
+clinical_df <- read.table("data/clinical_df.tsv", header=T, stringsAsFactors=FALSE)
+seqfish_df <- read.table("data/seqfish_df.tsv",header=T)
