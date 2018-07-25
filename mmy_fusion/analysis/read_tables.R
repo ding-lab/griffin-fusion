34271c8f20bacd54bcf00f799fea4db69ebe4382
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-07-25 10:04:27 -0500

    Add ggrepel library

--- mmy_fusion/analysis/read_tables.R
+++ mmy_fusion/analysis/read_tables.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(ggrepel)
 
 fusion_df <- read.table("data/fusion_df.txt", header=T, stringsAsFactors=F)
 all_samples <- read.table("data/sample_list.806.txt", header=F, stringsAsFactors=F)
@@ -9,10 +10,10 @@ expr <- read.table("data/mmy_gene_expr_with_fusions.tsv", header=T, stringsAsFac
 clinical_df <- read.table("data/clinical_df.tsv", header=T, stringsAsFactors=F)
 seqfish_df <- read.table("data/seqfish_df.tsv", header=T, stringsAsFactors=F)
 
+ericsript_twocallers <- c("EF", "EI", "EP", "ES")
 bad_gene_list <- c("FOSB")
-for(f in unique(subset(fusion_df, chrA == chrB & strandA == strandB)$fusion)){
-  mean_distance <- mean(abs(subset(fusion_df, fusion == f)$posA - subset(fusion_df, fusion == f)$posB))
-  print(mean_distance)
-}
 readthrough_fusion_list <- names(sort(table(subset(fusion_df, chrA == chrB & strandA == strandB & abs(posA-posB) < 250000)$fusion), decreasing=TRUE))
-primary_df <- subset(fusion_df, sample_number==1 & CallerN >= 2 & !( Callers %in% c("EF", "EI", "EP", "ES")) & !(geneA %in% bad_gene_list) & !(geneB %in% bad_gene_list) & !(fusion %in% readthrough_fusion_list) )
+
+fosb_df <- subset(fusion_df, sample_number==1 & CallerN >= 2 & !( Callers %in% ericsript_twocallers) & geneA=="FOSB")
+readthrough_df <- subset(fusion_df, sample_number==1 & CallerN >= 2 & !( Callers %in% ericsript_twocallers) & !(geneA %in% bad_gene_list) & !(geneB %in% bad_gene_list) & (fusion %in% readthrough_fusion_list) )
+primary_df <- subset(fusion_df, sample_number==1 & CallerN >= 2 & !( Callers %in% ericsript_twocallers) & !(geneA %in% bad_gene_list) & !(geneB %in% bad_gene_list) & !(fusion %in% readthrough_fusion_list) )
