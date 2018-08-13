1ab192a339bdd15093c7156fc4c89f18375ce629
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-08-13 16:57:04 -0500

    Update clinical data file

--- mmy_fusion/data/retrieve_data.sh
+++ mmy_fusion/data/retrieve_data.sh
@@ -14,12 +14,12 @@ rsync -avh 'sfoltz@linus6.gsc.wustl.edu:/gscmnt/gc2737/ding/Analysis/RNA-seq/fus
   /gscmnt/gc2737/ding/Analysis/RNA-seq/fusion_paper/00_filtering_annotation/02_sv_annotation/Filtered_Fusions_100000_delly_manta_20180813.txt \
   /gscmnt/gc2737/ding/Analysis/RNA-seq/fusion_paper/00_filtering_annotation/03_cnv_annotation/all_output/combined_cnv_results.txt \
   /gscmnt/gc2737/ding/Analysis/RNA-seq/Gene_Expression/kallisto_tpm/out4/mmy_gene_tpm_table.tsv \
+  /gscmnt/gc2737/ding/sample_info/clinical_data/clinical_data.20180813/Clinical_data.20180813.csv \
   /gscmnt/gc2737/ding/sample_info/SeqFISH.20180104.csv \
-  /gscmnt/gc2737/ding/sample_info/clinical_data.20171201.csv \
   /gscmnt/gc2737/ding/Analysis/RNA-seq/fusion_paper/00_filtering_annotation/00_combined_fusion_file/FilterDatabase/oncogene.tsv \
   /gscmnt/gc2737/ding/Analysis/RNA-seq/fusion_paper/00_filtering_annotation/00_combined_fusion_file/FilterDatabase/tsg.tsv \
   /gscmnt/gc2737/ding/Analysis/RNA-seq/fusion_paper/00_filtering_annotation/00_combined_fusion_file/FilterDatabase/mmy_known.tsv \
   /gscmnt/gc2737/ding/Analysis/RNA-seq/fusion_paper/00_filtering_annotation/00_combined_fusion_file/FilterDatabase/driver.tsv \
   /gscmnt/gc2737/ding/Analysis/RNA-seq/fusion_paper/00_filtering_annotation/00_combined_fusion_file/FilterDatabase/kinase.tsv ' .
 
-
+#  /gscmnt/gc2737/ding/sample_info/clinical_data.20171201.csv \
