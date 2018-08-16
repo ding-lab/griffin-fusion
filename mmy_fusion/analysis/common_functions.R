f7abca47756df204268d2cd8b30c193c16d80dcd
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-08-16 16:30:05 -0500

    List of functions used across many scripts

---
+++ mmy_fusion/analysis/common_functions.R
@@ -0,0 +1,11 @@
+# ==============================================================================
+# Set of commonly used functions repeated through different scripts
+# Steven Foltz (smfoltz@wustl.edu), August 2018
+# ==============================================================================
+
+# ==============================================================================
+# Elements added to end of many ggplot2 statements
+# ==============================================================================
+ggplot2_standard_additions <- function(){
+  theme_bw(base_size = 20)
+}
