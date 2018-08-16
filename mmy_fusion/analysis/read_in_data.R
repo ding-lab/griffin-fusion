b2aabd1786f45429bf11b52132a1fe471b7bdf77
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-08-16 14:13:38 -0500

    Reformat script for easy reading

--- mmy_fusion/analysis/read_in_data.R
+++ mmy_fusion/analysis/read_in_data.R
@@ -1,20 +1,36 @@
-# Read in data for analysis
+# ==============================================================================
+# Reads in data for analysis
+# Steven Foltz (smfoltz@wustl.edu)
+# ==============================================================================
 
+# ==============================================================================
 # Load all pacakges at beginning
+# ==============================================================================
 library(tidyverse)
 
+# ==============================================================================
 # Sample lists (MMRF, SRR) of data related to samples in this analysis
+# ==============================================================================
 samples_all <- read_tsv("data/sample_list.806.txt", col_names = c("MMRF","SRR"))
-samples_primary <- read_tsv("data/sample_list.primary.txt", col_names = c("MMRF","SRR"))
+samples_primary <- read_tsv("data/sample_list.primary.txt", 
+                            col_names = c("MMRF","SRR"))
 
+# ==============================================================================
 # Fusion tibbles. fusions_primary contains fusions from primary time points
+# ==============================================================================
 fusions_all <- read_tsv("data/fusion_df.txt")
 fusions_primary <- fusions_all %>% filter(srr %in% samples_primary$SRR)
 
+# ==============================================================================
 # seqFISH and clinical information
+# ==============================================================================
 seqfish_clinical_info <- read_tsv("data/seqfish_clinical.txt")
 
+# ==============================================================================
 # Gene expression data
-expr <- read_tsv("data/mmy_gene_expr_with_fusions.tsv")
-expr <- expr %>% filter(srr %in% samples_all$SRR) # only keep data from samples in analysis
-primary_expr <- expr %>% filter(srr %in% samples_primary$SRR) # filter out non-primary samples
+# ==============================================================================
+# read in expression data but only keep data from samples in analysis
+expression_all <- read_tsv("data/mmy_gene_expr_with_fusions.tsv") %>%
+  filter(srr %in% samples_all$SRR)
+# keep primary samples only
+expression_primary <- expression_all %>% filter(srr %in% samples_primary$SRR)
