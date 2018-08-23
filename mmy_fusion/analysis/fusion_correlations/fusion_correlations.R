382ea40a8af6a5267bb70544d36efac6208cbb68
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-08-23 16:27:10 -0500

    Scripts to analyze correlation of fusion status with expression and clinical features

---
+++ mmy_fusion/analysis/fusion_correlations/fusion_correlations.R
@@ -0,0 +1,73 @@
+# ==============================================================================
+# Measure associations between fusion status and expression and clinical info
+# Steven Foltz (smfoltz@wustl.edu), August 2018
+# ==============================================================================
+
+# ==============================================================================
+# Functions
+# ==============================================================================
+
+# Test gene expression for both genes in a fusion pair
+test_fusion_expression <- function(this_fusion, fusions_tbl, expression_tbl){
+  geneA = str_split(this_fusion, pattern = "--", simplify = TRUE)[1]
+  geneB = str_split(this_fusion, pattern = "--", simplify = TRUE)[2]
+  return(c(
+    test_gene_expression(geneA, fusions_tbl, expression_tbl),
+    test_gene_expression(geneA, fusions_tbl, expression_tbl)
+  ))
+}
+
+# Returns median expression percentile, t-test, and Fisher's Exact Test p-values
+test_gene_expression <- function(this_gene, fusions_tbl, expression_tbl){
+  # Given a gene, find out if expression differs between fusion / not fusion
+  samples_with_fusion <- fusions_tbl %>% filter(geneA == this_gene | geneB == this_gene) %>% select(srr)
+  expression_with_fusion <- expression_tbl %>% semi_join(samples_with_fusion, by = "srr") %>%
+    filter(gene == this_gene)
+  expression_without_fusion <- expression_tbl %>% anti_join(samples_with_fusion, by = "srr") %>%
+    filter(gene == this_gene)
+  
+  # median expression percentile of fusion samples
+  median_pct <- as.double(expression_with_fusion %>% summarize(median(pct)))
+  
+  # t-test
+  if ( median_pct >= 0.5 ) {
+    t.test.pvalue <- t.test(expression_with_fusion$log10tpm, expression_without_fusion$log10tpm, alternative = "greater")$p.value
+    over_table <- expression_primary %>% 
+      group_by(has_fusion = srr %in% samples_with_fusion$srr) %>%
+      filter(gene == this_gene) %>% select(has_fusion, outlier_over_tpm) %>%
+      summarize(outlier = sum(outlier_over_tpm == 1), not_outlier = sum(outlier_over_tpm == 0)) %>%
+      select(outlier, not_outlier)
+    fisher.pvalue <- fisher.test(over_table, alternative = "t")$p.value
+  } else{
+    t.test.pvalue <- t.test(expression_with_fusion$log10tpm, expression_without_fusion$log10tpm, alternative = "less")$p.value
+    under_table <- expression_primary %>% 
+      group_by(has_fusion = srr %in% samples_with_fusion$srr) %>%
+      filter(gene == this_gene) %>% select(has_fusion, outlier_under_tpm) %>%
+      summarize(outlier = sum(outlier_under_tpm == 1), not_outlier = sum(outlier_under_tpm == 0)) %>%
+      select(outlier, not_outlier)
+    fisher.pvalue <- fisher.test(under_table, alternative = "t")$p.value
+    
+  }
+  return( c(median_pct, t.test.pvalue, fisher.pvalue))
+}
+
+# ==============================================================================
+# Prepare lists of fusions and genes for testing (avoid testing rare events)
+# ==============================================================================
+
+# Fusions pairs seen in at least 3 samples
+fusion_pairs_gt2 <- fusions_primary %>% group_by(fusion) %>% 
+  summarize(count = n()) %>% filter(count >= 3) %>% arrange(desc(count))
+
+# Fusion genes seen in at least 3 samples
+fusion_genes_gt2 <- fusions_primary %>% gather(geneA, geneB, key = "geneAB", value = "fusion_gene") %>%
+  group_by(fusion_gene) %>% filter( !(fusion_gene %in% c("IGH@", "IGK@", "IGL@")) ) %>%
+  summarize(count = n()) %>% filter(count >= 3) %>% arrange(desc(count))
+
+# ==============================================================================
+# ==============================================================================
+
+for (this_gene in fusion_genes_gt2$fusion_gene) {
+  print(this_gene)
+  print(c(this_gene, test_expression(this_gene, fusions_primary, expression_primary)))
+}
