ad9703ba8d39c6bdc3776b6c0bc3b7dbbefea35f
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-08-28 17:15:37 -0500

    Change MMRF and SRR column headers to mmrf and srr

--- mmy_fusion/analysis/read_in_data.R
+++ mmy_fusion/analysis/read_in_data.R
@@ -12,15 +12,15 @@ source("analysis/common_functions.R")
 # ==============================================================================
 # Sample lists (MMRF, SRR) of data related to samples in this analysis
 # ==============================================================================
-samples_all <- read_tsv("data/sample_list.806.txt", col_names = c("MMRF","SRR"))
+samples_all <- read_tsv("data/sample_list.806.txt", col_names = c("mmrf","srr"))
 samples_primary <- read_tsv("data/sample_list.primary.txt", 
-                            col_names = c("MMRF","SRR"))
+                            col_names = c("mmrf","srr"))
 
 # ==============================================================================
 # Fusion tibbles. fusions_primary contains fusions from primary time points
 # ==============================================================================
 fusions_all <- read_tsv("data/fusion_df.txt")
-fusions_primary <- fusions_all %>% filter(srr %in% samples_primary$SRR)
+fusions_primary <- fusions_all %>% filter(srr %in% samples_primary$srr)
 
 # ==============================================================================
 # seqFISH and clinical information
@@ -32,6 +32,6 @@ seqfish_clinical_info <- read_tsv("data/seqfish_clinical.txt")
 # ==============================================================================
 # read in expression data but only keep data from samples in analysis
 expression_all <- read_tsv("data/mmy_gene_expr_with_fusions.tsv") %>%
-  filter(srr %in% samples_all$SRR)
+  filter(srr %in% samples_all$srr)
 # keep primary samples only
-expression_primary <- expression_all %>% filter(srr %in% samples_primary$SRR)
+expression_primary <- expression_all %>% filter(srr %in% samples_primary$srr)
