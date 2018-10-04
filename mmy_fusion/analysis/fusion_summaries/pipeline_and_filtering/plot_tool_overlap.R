d97b197616f27afd40fc93cb7bf23433443a8f9f
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-10-04 12:51:42 -0500

    Make upsetr plot wider

--- mmy_fusion/analysis/fusion_summaries/pipeline_and_filtering/plot_tool_overlap.R
+++ mmy_fusion/analysis/fusion_summaries/pipeline_and_filtering/plot_tool_overlap.R
@@ -3,6 +3,7 @@
 # Steven Foltz (smfoltz@wustl.edu), September 2018
 # ==============================================================================
 
+plot_dir = "analysis/fusion_summaries/pipeline_and_filtering/"
 # ==============================================================================
 # Functions
 # ==============================================================================
@@ -27,7 +28,7 @@ fusions_primary %>%
   geom_label(aes(label = tool_key, x = 1, y = 150), hjust = 0, vjust = 0) +
   ggplot2_standard_additions()
 
-ggsave("analysis/fusion_summaries/tool_overlap.bar_chart.pdf", device = "pdf", 
+ggsave(str_c(plot_dir, "tool_overlap.bar_chart.pdf"), device = "pdf", 
        width = 10, height = 10)
 
 # ==============================================================================
@@ -35,10 +36,11 @@ ggsave("analysis/fusion_summaries/tool_overlap.bar_chart.pdf", device = "pdf",
 # ==============================================================================
 library(UpSetR)
 
-plot_df <- fusions_primary %>% select(starts_with("called_by")) %>% data.frame()
-names(plot_df) <- c("EricScript", "FusionCatcher", "INTEGRATE", 
+upsetr_df <- fusions_primary %>% select(starts_with("called_by")) %>% data.frame()
+names(upsetr_df) <- c("EricScript", "FusionCatcher", "INTEGRATE", 
 "PRADA", "STAR-Fusion")
-pdf(file = "analysis/fusion_summaries/tool_overlap.upsetr.pdf", 
-    width = 10, height = 10)
-upset(plot_df, order.by = "freq")
+pdf(file = str_c(plot_dir, "tool_overlap.upsetr.pdf"), 
+    width = 20, height = 10)
+upset(upsetr_df, nsets = ncol(upsetr_df), nintersects = NA, order.by = "freq",
+      text.scale = 2, point.size = 3, line.size = 1)
 dev.off()
