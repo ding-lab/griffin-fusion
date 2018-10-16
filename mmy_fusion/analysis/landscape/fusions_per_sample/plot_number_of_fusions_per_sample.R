f7a1746e3b134a43748c14c944b19b85b27338ac
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-10-16 17:02:32 -0500

    Compare hyper and non-hyper diploidy groups

--- mmy_fusion/analysis/landscape/fusions_per_sample/plot_number_of_fusions_per_sample.R
+++ mmy_fusion/analysis/landscape/fusions_per_sample/plot_number_of_fusions_per_sample.R
@@ -1,21 +1,27 @@
 # ==============================================================================
-# Create a plot number of fusions per sample
+# Create a plot of number of fusions per sample
 # Steven Foltz (smfoltz@wustl.edu), August 2018
 # ==============================================================================
+
+plot_dir = "analysis/landscape/fusions_per_sample/"
 
 # ==============================================================================
 # Function to obtain number of fusions per sample
 # ==============================================================================
 n_fusions_per_sample <- function(fusions_df){
-  tibble( mmrf = names(table(fusions_df$mmrf)), n_fusions = as.integer(unname(table(fusions_df$mmrf))) )
+  tibble( mmrf = names(table(fusions_df$mmrf)), 
+          n_fusions = as.integer(unname(table(fusions_df$mmrf))) )
 }
 
 # ==============================================================================
 # Create data frame for plotting
 # ==============================================================================
-n_hdp <- seqfish_clinical_info %>% filter(seqfish_Hyperdiploidy == 1) %>% nrow()
-n_not_hdp <- seqfish_clinical_info %>% filter(seqfish_Hyperdiploidy == 0) %>% nrow()
-n_na_hdp <- seqfish_clinical_info %>% filter(is.na(seqfish_Hyperdiploidy)) %>% nrow()
+n_hdp <- seqfish_clinical_info %>% 
+  filter(seqfish_Hyperdiploidy == 1) %>% nrow()
+n_not_hdp <- seqfish_clinical_info %>% 
+  filter(seqfish_Hyperdiploidy == 0) %>% nrow()
+n_na_hdp <- seqfish_clinical_info %>% 
+  filter(is.na(seqfish_Hyperdiploidy)) %>% nrow()
 hpd_key <- tribble(~seqfish_Hyperdiploidy, ~hyperdiploid_categories, ~count,
                0, str_c("Not Hyperdiploid (", n_not_hdp, ")"), n_not_hdp,
                1, str_c("Hyperdiploid (", n_hdp, ")"), n_hdp,
@@ -30,11 +36,13 @@ plot_df <- seqfish_clinical_info %>% select(mmrf, seqfish_Hyperdiploidy) %>%
 # ==============================================================================
 # Plot number of fusions per sample
 # ==============================================================================
-plot_df %>% ggplot(aes(x = n_fusions)) + geom_histogram(binwidth = 1, center = 0) + 
+plot_df %>% 
+  ggplot(aes(x = n_fusions)) + 
+  geom_histogram(binwidth = 1, center = 0) + 
   facet_wrap(~ fct_reorder(hyperdiploid_categories, -count), ncol = 1) +
   labs(x = "Number of fusions detected", y = "Number of samples") +
   ggplot2_standard_additions()
-ggsave("analysis/fusion_summaries/histogram_n_fusions_per_sample.pdf", device = "pdf", 
+ggsave(str_c(plot_dir, "histogram_n_fusions_per_sample.pdf"), device = "pdf", 
        width = 10, height = 10)
 
 # ==============================================================================
@@ -46,5 +54,67 @@ plot_df %>% ggplot(aes(x = n_fusions, y = ..density..)) +
   labs(x = "Number of fusions detected", y = "Proportion of samples",
        color = "Hyperdiploid Category") +
   ggplot2_standard_additions()
-ggsave("analysis/fusion_summaries/freqpoly_n_fusions_per_sample.pdf", device = "pdf", 
+ggsave(str_c(plot_dir, "freqpoly_n_fusions_per_sample.pdf"), device = "pdf", 
        width = 10, height = 10)
+
+# ==============================================================================
+# Median number of fusions per category
+# ==============================================================================
+n_fusion_tibble <- plot_df %>% group_by(hyperdiploid_categories) %>% 
+  summarize(min_n_fusions = min(n_fusions), 
+            median_n_fusions = median(n_fusions), 
+            mean_n_fusions = mean(n_fusions), 
+            max_n_fusions = max(n_fusions))
+
+write_tsv(n_fusion_tibble, str_c(plot_dir, "n_fusion_table.txt"), 
+          na = "NA", append = FALSE, col_names = TRUE)
+
+# ==============================================================================
+# Characteristics of samples with large number of fusions
+# Not really a defined pattern that separtes outliers from non-outliers
+# ==============================================================================
+
+outlier_n_fusions <- fusions_primary %>% group_by(srr) %>% 
+  summarize(n = n()) %>% 
+  mutate(z = (n - mean(n))/sd(n) ) %>% 
+  filter(abs(z) > 2) %>% pull(n) %>% min()
+
+mmrf_with_large_n <- plot_df %>% 
+  filter(n_fusions >= outlier_n_fusions) %>% pull(mmrf)
+
+mean_igl_nonoutliers <- mean(fusions_primary %>%
+                               filter(!(mmrf %in% mmrf_with_large_n)) %>% 
+                               mutate(geneB_IGL = geneB == "IGL@") %>% 
+                               pull(geneB_IGL))
+
+mean_igl_outliers <- mean(fusions_primary %>%
+                            filter(mmrf %in% mmrf_with_large_n) %>%
+                            mutate(geneB_IGL = geneB == "IGL@") %>% 
+                            pull(geneB_IGL))
+
+# ==============================================================================
+# Characteristics of samples with zero fusions
+# ==============================================================================
+
+mmrf_zero_fusions <- samples_primary %>% 
+  left_join(fusions_primary, by = "mmrf") %>%
+  filter(is.na(srr.y)) %>% pull(mmrf)
+
+# ==============================================================================
+# t-test to compare number of fusions between hyperdiploidy and not
+# NOT significant difference by t-test
+# ==============================================================================
+
+n_fusions_with_hyperdiploid_info <- seqfish_clinical_info %>% 
+  left_join(fusions_primary, by = "mmrf") %>% 
+  filter(!is.na(seqfish_Hyperdiploidy)) %>% 
+  group_by(mmrf, srr, seqfish_Hyperdiploidy)  %>% 
+  summarize(n = n()) %>% 
+  ungroup() %>% 
+  mutate(n_corrected = n - is.na(srr))
+
+n_fusions_hyperdiploid <- n_fusions_with_hyperdiploid_info %>% 
+  filter(seqfish_Hyperdiploidy == 1) %>% pull(n_corrected)
+n_fusions_nonhyperdiploid <- n_fusions_with_hyperdiploid_info %>% 
+  filter(seqfish_Hyperdiploidy == 0) %>% pull(n_corrected)
+t.test(n_fusions_hyperdiploid, n_fusions_nonhyperdiploid)
