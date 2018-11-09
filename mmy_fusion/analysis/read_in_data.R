a3083e4f4a45087cb243f535c68a6b81a38bfb48
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-11-09 15:57:39 -0600

    Read in TCGA fusions xlsx file

--- mmy_fusion/analysis/read_in_data.R
+++ mmy_fusion/analysis/read_in_data.R
@@ -7,13 +7,14 @@
 # Load all packages at beginning
 # ==============================================================================
 library(tidyverse)
+library(readxl)
 source("analysis/common_functions.R")
 
 # ==============================================================================
 # Sample lists (MMRF, SRR) of data related to samples in this analysis
 # ==============================================================================
 samples_all <- read_tsv("data/sample_list.806.txt", col_names = c("mmrf","srr"))
-samples_primary <- read_tsv("data/sample_list.primary.txt", 
+samples_primary <- read_tsv("data/sample_list.primary.txt",
                             col_names = c("mmrf","srr"))
 
 # ==============================================================================
@@ -23,7 +24,7 @@ fusions_all <- read_tsv("data/fusion_df.txt")
 fusions_primary <- fusions_all %>% filter(srr %in% samples_primary$srr)
 
 fusions_hard_all <- read_tsv("data/Hard_Filtered_Fusions.tsv")
-fusions_hard_primary <- fusions_hard_all %>% 
+fusions_hard_primary <- fusions_hard_all %>%
   filter(Sample %in% samples_primary$srr)
 
 # ==============================================================================
@@ -43,7 +44,7 @@ expression_primary <- expression_all %>% filter(srr %in% samples_primary$srr)
 # ==============================================================================
 # Sample output file locations
 # ==============================================================================
-file_locations <- read_tsv("data/sample_list.with_file_names.txt", 
+file_locations <- read_tsv("data/sample_list.with_file_names.txt",
                            col_names = FALSE)
 
 # ==============================================================================
@@ -51,3 +52,11 @@ file_locations <- read_tsv("data/sample_list.with_file_names.txt",
 # ==============================================================================
 # read in list of ENSGs and gene names used in this study
 ensg_gene_list <- read_tsv("data/ensg_gene_list.tsv")
+
+# ==============================================================================
+# TCGA Pan-cancer fusion analysis results
+# ==============================================================================
+pancan_fusions <- read_excel("data/tcga_pancancer_fusions.xlsx",
+                             sheet = "Final fusion call set")
+names(pancan_fusions) <- pancan_fusions[1,]
+pancan_fusions <- pancan_fusions[-1,]
