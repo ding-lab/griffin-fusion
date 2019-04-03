6f4fea8e655b4721aaaadad11ebf118c0c1f0371
Steven Foltz <stevenmasonfoltz@gmail.com>
2019-04-03 14:19:59 -0500

    Filter out significantly undervalidated fusions and remove @ symbol from IG genes

--- mmrf_fusion/analysis/read_in_data.R
+++ mmrf_fusion/analysis/read_in_data.R
@@ -13,19 +13,51 @@ source("analysis/common_functions.R")
 # ==============================================================================
 # Sample lists (MMRF, SRR) of data related to samples in this analysis
 # ==============================================================================
-samples_all <- read_tsv("data/sample_list.806.txt", col_names = c("mmrf","srr"))
-samples_primary <- read_tsv("data/sample_list.primary.txt",
+samples_all <- read_tsv("data/sample_list.806.txt", 
+                        col_names = c("mmrf","srr"))
+samples_primary <- read_tsv("data/sample_list.primary.txt", header = FALSE,
                             col_names = c("mmrf","srr"))
 
 # ==============================================================================
 # Fusion tibbles. fusions_primary contains fusions from primary time points
+# This is BEFORE removing significantly undervalidated fusions
 # ==============================================================================
-fusions_all <- read_tsv("data/fusion_df.txt")
+fusions_all <- read_tsv("data/fusion_df.txt") %>%
+  mutate(fusion = str_remove(fusion, "@"),
+         geneA = str_remove(geneA, "@"),
+         geneB = str_remove(geneB, "@"))
 fusions_primary <- fusions_all %>% filter(srr %in% samples_primary$srr)
 
-fusions_hard_all <- read_tsv("data/Hard_Filtered_Fusions.tsv")
-fusions_hard_primary <- fusions_hard_all %>%
-  filter(Sample %in% samples_primary$srr)
+#fusions_hard_all <- read_tsv("data/Hard_Filtered_Fusions.tsv")
+#fusions_hard_primary <- fusions_hard_all %>%
+#  filter(Sample %in% samples_primary$srr)
+
+# ==============================================================================
+# Flag and remove potential false positives (significantly undervalidated)
+# This REMOVES significantly undervalidated fusions from fusions data frames
+# ==============================================================================
+wgs_discordant_read_validation_rate <- 
+fusions_primary %>% 
+  filter(!is.na(n_discordant), 
+         Overlap != "Overlapping_regions") %>% 
+  mutate(validated = n_discordant >= 1) %>%
+  pull(validated) %>% mean()
+
+significantly_under_validated_fusions <- fusions_primary %>% 
+  filter(!is.na(n_discordant), 
+         Overlap != "Overlapping_regions") %>% 
+  mutate(validated = n_discordant >= 1) %>% 
+  group_by(fusion) %>% 
+  summarize(n = n(), n_validated = sum(validated)) %>%
+  arrange(desc(n)) %>%
+  mutate(p_value = pbinom(q = n_validated, size = n, 
+                          prob = wgs_discordant_read_validation_rate)) %>% 
+  filter(p_value < 0.15) %>% pull(fusion)
+
+fusions_all <- fusions_all %>% 
+  filter(!(fusion %in% significantly_under_validated_fusions))
+fusions_primary <- fusions_primary %>% 
+  filter(!(fusion %in% significantly_under_validated_fusions))
 
 # ==============================================================================
 # seqFISH and clinical information
@@ -44,8 +76,8 @@ expression_primary <- expression_all %>% filter(srr %in% samples_primary$srr)
 # ==============================================================================
 # Sample output file locations
 # ==============================================================================
-file_locations <- read_tsv("data/sample_list.with_file_names.txt",
-                           col_names = FALSE)
+#file_locations <- read_tsv("data/sample_list.with_file_names.txt",
+#                           col_names = FALSE)
 
 # ==============================================================================
 # ENSG gene names
