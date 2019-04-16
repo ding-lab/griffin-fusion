5d7408a5b700ca25dbf8b3fd7ea1cbf323315bb5
Steven Foltz <stevenmasonfoltz@gmail.com>
2019-04-16 11:57:20 -0500

    Study expression of genes involved in fusions

---
+++ mmrf_fusion/analysis/02_expression/02_expression.R
@@ -0,0 +1,11 @@
+# ==============================================================================
+# Expression (MMRF Fusions)
+# Steven Foltz (smfoltz@wustl.edu), April 2019
+# ==============================================================================
+
+paper_main = "paper/main/02_expression/"
+paper_supp = "paper/supplemental/02_expression/"
+
+# Create directories 
+dir.create(paper_main, recursive = TRUE, showWarnings = FALSE)
+dir.create(paper_supp, recursive = TRUE, showWarnings = FALSE)
