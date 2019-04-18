136e9860b655087987d9b4bb2267e06c4d492cd3
Steven Foltz <stevenmasonfoltz@gmail.com>
2019-04-18 11:40:36 -0500

    Scripts to analyze clinical data and associations

---
+++ mmrf_fusion/analysis/05_clinical/05_clinical.R
@@ -0,0 +1,11 @@
+# ==============================================================================
+# Clinical (MMRF Fusions)
+# Steven Foltz (smfoltz@wustl.edu), April 2019
+# ==============================================================================
+
+paper_main = "paper/main/05_clinical/"
+paper_supp = "paper/supplemental/05_clinical/"
+
+# Create directories 
+dir.create(paper_main, recursive = TRUE, showWarnings = FALSE)
+dir.create(paper_supp, recursive = TRUE, showWarnings = FALSE)
