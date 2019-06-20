b685caaf37b7358b7db6a30f4596291442b662f2
Steven Foltz <stevenmasonfoltz@gmail.com>
2019-06-20 12:28:14 -0500

    Remove those nasty QC samples

--- mmrf_fusion/data/retrieve_data.sh
+++ mmrf_fusion/data/retrieve_data.sh
@@ -32,14 +32,12 @@ mv mmc2.xlsx tcga_pancancer_fusions.xlsx
 # single cell analysis #
 ########################
 rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/QCtesting/bad_qc_reads_plot_df.tsv' .
-
 rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/lyao/Tools/GRCh38_gencode_v29_CTAT_lib_Mar272019.plug-n-play/ctat_genome_lib_build_dir/ref_annot.gtf.gene_spans' .
 
 #27522_1 
-#rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/27522_1/5000.discordant_reads.tsv' scRNA.discordant_reads.27522_1.tsv
 rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/27522_1/chr4chr14.discovered_discordant_reads.tsv ' scRNA.discordant_reads.discover.27522_1.tsv
-#rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/scRNA.27522_1.bulk.Chimeric.out.junction' .
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/lyao/MMY_RNAseq/STAR-Fusion/27522_1.ANNOTATS/star-fusion.fusion_predictions.tsv' scRNA.27522_1.bulk.star-fusion.fusion_predictions.tsv
+rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/scRNA.27522_1.bulk.Chimeric.out.junction' .
+rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Datasets/mmy_scratch/Organized/27522_1/BulkRNASeq/27522_1.ANNOTATS/star-fusion.fusion_predictions.tsv' scRNA.27522_1.bulk.star-fusion.fusion_predictions.tsv
 rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/scRNA.27522_1.bulk.chr414.Chimeric.out.junction' .
 rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/lyao/MMY_scRNA/inferCNV/27522_1_subcluster/V3/27522_raw_counts.cutoff.0.1/infercnv.observations.txt' scRNA.27522_1.infercnv.observations.txt
 rsync -avh 'sfoltz@denali.wusm.wustl.edu:/diskmnt/Projects/Users/qgao/Priority/Gao_scRNA_cell_type/2.Prefiltered_celltype/backup_object_cell_type_in_sample_27522_1.rds ' scRNA.seurat_object.27522_1.rds
@@ -50,44 +48,10 @@ rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-
 rsync -avh 'sfoltz@denali.wusm.wustl.edu:/diskmnt/Projects/Users/qgao/Priority/Gao_scRNA_cell_type/2.Prefiltered_celltype/backup_object_cell_type_in_sample_27522_4.rds ' scRNA.seurat_object.27522_4.rds
 rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/lyao/MMY_scRNA/inferCNV/27522_4_subcluster/V2/27522_4_raw_counts.cutoff.0.1/infercnv.observations.txt' scRNA.27522_4.infercnv.observations.txt
 
-#56203_1
-rsync -avh 'sfoltz@denali.wusm.wustl.edu:/diskmnt/Projects/Users/qgao/Priority/Gao_scRNA_cell_type/2.Prefiltered_celltype/backup_object_cell_type_in_sample_56203_1.rds ' scRNA.seurat_object.56203_1.rds
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/56203_1/chr8chr14.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.56203_1.MYC_IGH.tsv
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/56203_1/chr8chr22.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.56203_1.MYC_IGL.tsv
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/56203_1/chr8chr2.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.56203_1.MYC_IGK.tsv
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/lyao/MMY_scRNA/inferCNV/56203_1_subcluster/56203_1_raw_counts.cutoff.0.1/infercnv.observations.txt' scRNA.56203_1.infercnv.observations.txt
-
-#56203_2
-rsync -avh 'sfoltz@denali.wusm.wustl.edu:/diskmnt/Projects/Users/qgao/Priority/Gao_scRNA_cell_type/2.Prefiltered_celltype/backup_object_cell_type_in_sample_56203_2.rds ' scRNA.seurat_object.56203_2.rds
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/56203_2/chr8chr14.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.56203_2.MYC_IGH.tsv
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/56203_2/chr8chr22.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.56203_2.MYC_IGL.tsv
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/56203_2/chr8chr2.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.56203_2.MYC_IGK.tsv
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/scRNA.56203_2.bulk.chr8chr21422.Chimeric.out.junction' .
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/lyao/MMY_scRNA/inferCNV/56203_2_subcluster/56203_2_raw_counts.cutoff.0.1/infercnv.observations.txt' scRNA.56203_2.infercnv.observations.txt
-
-#81012_1
-rsync -avh 'sfoltz@denali.wusm.wustl.edu:/diskmnt/Projects/Users/qgao/Priority/Gao_scRNA_cell_type/2.Prefiltered_celltype/backup_object_cell_type_in_sample_81012_1.rds ' scRNA.seurat_object.81012_1.rds
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/81012_1/chr11chr14.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.81012_1.tsv
-
-#81012_2
-rsync -avh 'sfoltz@denali.wusm.wustl.edu:/diskmnt/Projects/Users/qgao/Priority/Gao_scRNA_cell_type/2.Prefiltered_celltype/backup_object_cell_type_in_sample_81012_2.rds ' scRNA.seurat_object.81012_2.rds
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/81012_2/chr11chr14.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.81012_2.tsv
-
-#47499
-rsync -avh 'sfoltz@denali.wusm.wustl.edu:/diskmnt/Projects/Users/qgao/Priority/Gao_scRNA_cell_type/2.Prefiltered_celltype/backup_object_cell_type_in_sample_47499_p.rds ' scRNA.seurat_object.47499.rds
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/47499/chr11chr14.discovered_discordant_reads.tsv' scRNA.discordant_reads.discover.47499.tsv
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/scRNA.47499.bulk.chr1114.Chimeric.out.junction' .
-
-#77570
-rsync -avh 'sfoltz@denali.wusm.wustl.edu:/diskmnt/Projects/Users/qgao/Priority/Gao_scRNA_cell_type/2.Prefiltered_celltype/backup_object_cell_type_in_sample_77570.rds ' scRNA.seurat_object.77570.rds
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/results/77570/chr11chr14.discovered_discordant_reads.tsv ' scRNA.discordant_reads.discover.77570.tsv
-rsync -avh 'sfoltz@katmai.wusm.wustl.edu:/diskmnt/Projects/Users/sfoltz/griffin-fusion/mmrf_fusion/analysis/06_single_cell/scRNA.77570.bulk.chr1114.Chimeric.out.junction ' .
-
-# convert inferCNV data to better format
-for x in 27522_1 27522_4 56203_1 56203_2; do
-  sed -i '1s/^/gene /g' scRNA.$x\.infercnv.observations.txt
-  sed -i 's/\s\+/\t/g' scRNA.$x\.infercnv.observations.txt
-done
+#for x in 27522_1 27522_4; do
+#  sed -i '1s/^/gene /g' scRNA.$x\.infercnv.observations.txt
+#  sed -i 's/\s\+/\t/g' scRNA.$x\.infercnv.observations.txt
+#done
 
 ########################
 # multiple time points #
