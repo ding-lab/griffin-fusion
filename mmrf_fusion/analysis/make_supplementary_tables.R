37e80f5a0f0a17fd23b387d78a46e490667dd019
Steven Foltz <stevenmasonfoltz@gmail.com>
2019-08-29 13:34:29 -0500

    Use Supplementary Table instead of Table S

--- mmrf_fusion/analysis/make_supplementary_tables.R
+++ mmrf_fusion/analysis/make_supplementary_tables.R
@@ -7,14 +7,14 @@ dir.create(supp_tables, recursive = TRUE, showWarnings = FALSE)
 
 # summary_tibble from 01_overview.R
 write_tsv(summary_tibble, 
-          str_c(supp_tables, "Table_S1.Patient_Info.tsv"), 
+          str_c(supp_tables, "Supplementary_Table_1.Patient_Info.tsv"), 
           na = "NA", append = FALSE, col_names = TRUE)
 
 # Table S2 fusions reported from all samples
 write_tsv(fusions_all %>% 
             select(-wgs_bam) %>%
             select(-discordant_reads),
-          str_c(supp_tables, "Table_S2.Fusion_Calls.tsv"), 
+          str_c(supp_tables, "Supplementary_Table_2.Fusion_Calls.tsv"), 
           na = "NA", append = FALSE, col_names = TRUE)
 
 # Table S3 overexpressed fusions
@@ -25,7 +25,7 @@ overexpressed_table <- testing_tbl_pvalue_adjusted %>%
   select(-c("event2", "n_samples_with_tested", 
             "n_samples_without_tested", "n_samples_na_tested"))
 write_tsv(overexpressed_table, 
-          str_c(supp_tables, "Table_S3.Overexpressed.tsv"), 
+          str_c(supp_tables, "Supplementary_Table_3.Overexpressed.tsv"), 
           na = "NA", append = FALSE, col_names = TRUE)
 
 # Table S4 kinase information
@@ -36,5 +36,5 @@ kinase_table <- read_tsv("data/Kinase_fusion_info.txt") %>%
                                             Group == "TKL" ~ "Tyrosine Kinase-Like",
                                             TRUE ~ Group))
 write_tsv(kinase_table, 
-          str_c(supp_tables, "Table_S4.Kinases.tsv"), 
+          str_c(supp_tables, "Supplementary_Table_4.Kinases.tsv"), 
           na = "NA", append = FALSE, col_names = TRUE)
