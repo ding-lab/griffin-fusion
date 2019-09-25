547c33591f7f96b6705d02695e8246d02cb7e001
Steven Foltz <stevenmasonfoltz@gmail.com>
2019-09-25 15:09:02 -0500

    Create Table S2 for all sample IDs

--- mmrf_fusion/analysis/make_supplementary_tables.R
+++ mmrf_fusion/analysis/make_supplementary_tables.R
@@ -10,14 +10,19 @@ write_tsv(summary_tibble,
           str_c(supp_tables, "Supplementary_Table_1.Patient_Info.tsv"), 
           na = "NA", append = FALSE, col_names = TRUE)
 
-# Table S2 fusions reported from all samples
+# Table S2 samples included in this study
+write_tsv(samples_all,
+          str_c(supp_tables, "Supplementary_Table_2.Patients_Included.tsv"),
+          na = "NA", append = FALSE, col_names = TRUE)
+
+# Table S3 fusions reported from all samples
 write_tsv(fusions_all %>% 
             select(-wgs_bam) %>%
             select(-discordant_reads),
-          str_c(supp_tables, "Supplementary_Table_2.Fusion_Calls.tsv"), 
+          str_c(supp_tables, "Supplementary_Table_3.Fusion_Calls.tsv"), 
           na = "NA", append = FALSE, col_names = TRUE)
 
-# Table S3 overexpressed fusions
+# Table S4 overexpressed fusions
 overexpressed_table <- testing_tbl_pvalue_adjusted %>% 
   filter(fdr < 0.05 | median_value > 0.9, 
          event_type %in% c("Fusion Gene Expression", 
@@ -25,10 +30,10 @@ overexpressed_table <- testing_tbl_pvalue_adjusted %>%
   select(-c("event2", "n_samples_with_tested", 
             "n_samples_without_tested", "n_samples_na_tested"))
 write_tsv(overexpressed_table, 
-          str_c(supp_tables, "Supplementary_Table_3.Overexpressed.tsv"), 
+          str_c(supp_tables, "Supplementary_Table_4.Overexpressed.tsv"), 
           na = "NA", append = FALSE, col_names = TRUE)
 
-# Table S4 kinase information
+# Table S5 kinase information
 kinase_table <- read_tsv("data/Kinase_fusion_info.txt") %>% 
   mutate(Fusion = str_remove(Fusion, "@")) %>% 
   mutate(kinase_group_full_name = case_when(Group == "TK" ~ "Tyrosine Kinase",
@@ -36,10 +41,10 @@ kinase_table <- read_tsv("data/Kinase_fusion_info.txt") %>%
                                             Group == "TKL" ~ "Tyrosine Kinase-Like",
                                             TRUE ~ Group))
 write_tsv(kinase_table, 
-          str_c(supp_tables, "Supplementary_Table_4.Kinases.tsv"), 
+          str_c(supp_tables, "Supplementary_Table_5.Kinases.tsv"), 
           na = "NA", append = FALSE, col_names = TRUE)
 
-# Table S5 soft filtering
+# Table S6 soft filtering
 soft_filtered %>% 
   group_by(FusionName, filter) %>% 
   summarize(fusion_count = n()) %>% 
@@ -50,5 +55,5 @@ soft_filtered %>%
          "Fusion Count" = "fusion_count", 
          "Filter" = "filter") %>%
   arrange(desc(`Fusion Count`), `Filter`) %>%
-  write_tsv(str_c(supp_tables, "Supplementary_Table_5.Soft_Filtered_Fusions.tsv"),
+  write_tsv(str_c(supp_tables, "Supplementary_Table_6.Soft_Filtered_Fusions.tsv"),
             na = "NA", append = FALSE, col_names = TRUE)
